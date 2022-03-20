16f7fceea96ac6cc4d9f626e404199214ca5f6c4
run-JW <65688438+run-JW@users.noreply.github.com>
2022-03-20 15:50:58 +0900

    Create 3.c

---
+++ C/HackerRank/3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x, y;
+    float fx, fy;
+    
+	scanf("%d %d", &x, &y);
+    scanf("%f %f", &fx, &fy);
+    
+    printf("%d %d\n", x + y, x - y);
+    printf("%.1f %.1f", fx + fy, fx - fy);
+    
+    return 0;
+}
