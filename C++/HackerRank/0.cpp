9bf83e030e57929c4bb62c49620d6d55290ab26b
run-JW <65688438+run-JW@users.noreply.github.com>
2022-04-04 20:16:07 +0900

    Update 0.cpp

--- C++/HackerRank/0.cpp
+++ C++/HackerRank/0.cpp
@@ -13,9 +13,9 @@ int main() {
     
     // Print a string literal saying "Hello, World." to stdout using cout.
     cout << "Hello, World." << endl;
-    cout << input_string << endl;
-
+    
     // TODO: Write a line of code here that prints the contents of input_string to stdout.
+    cout << input_string << endl;
 
     return 0;
 }
