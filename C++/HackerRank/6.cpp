ad30a5067146e46ffd4b50e10ede204e702e3599
run-JW <65688438+run-JW@users.noreply.github.com>
2022-04-23 23:02:25 +0900

    Update 6.cpp

--- C++/HackerRank/6.cpp
+++ C++/HackerRank/6.cpp
@@ -5,10 +5,34 @@
 #include <algorithm>
 using namespace std;
 
-
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    int n;
-    cout << n << endl;
+    int input;
+    cin >> input;
+    
+    vector<string> arr(input);
+    
+    for (int i = 0; i < input; i++) {
+        string temp;
+        cin >> temp;
+        arr[i] = temp;
+    }
+    
+    for (int i = 0; i < input; i++) {
+        string mainStr = arr[i];
+        string temp1;
+        string temp2;
+     
+        for (int j = 0; j < mainStr.length(); j++) {
+            if (j % 2 == 1) {
+                temp2 += mainStr.at(j);
+            } else {
+                temp1 += mainStr.at(j);
+            }
+        }
+             
+        cout << temp1 << " " << temp2 << endl;
+    }
+    
     return 0;
 }
